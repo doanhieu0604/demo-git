3e87f49486b4174d7a40e34391a6455fb9a2054d
Doan Hieu <hieu06041997@gmail.com>
2020-06-21 11:00:56 +0700

    fix lõi

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 console.log("FE42");
 console.log("Doan Chau Thanh Hieu");
 console.log(" Hello New day");
 console.log ("Ngay moi vui ve");
-=======
-console.log("FE42")
-console.log("Doan Chau Thanh Hieu")
-console.log(" Hello New day")
-console.log("Demo git FE42")
->>>>>>> master
